daf1827cdb60afbcc4184f9514ff99d05e7a54eb
yeropharaoh <yeroschutte@gmail.com>
2019-03-07 18:47:30 +0100

    some changes

--- includes/script.js
+++ includes/script.js
@@ -1,12 +1,30 @@
+// SCROLLING
+function scrollAnchor (desiredHeight) {
+  $('.menu li a[href*="#"]').on("click", function(e) {
+      e.preventDefault(); // prevent hard jump
+      const target = $(this).attr("href"); 
+      
+      $('html, body').stop().animate({
+          scrollTop: $(target).offset().top - desiredHeight 
+      }, 600);
+      return false;
+  });
+};
+
+$(document).ready(() => {
+  scrollAnchor(10);
+});
+
+//TYPED.js
 document.addEventListener('DOMContentLoaded', function() {
   var typed = new Typed('#typed', {
-    strings: ['Hi!', 'I am: Yero &copy;', 'I am: <strong>an aspiring developer</strong>', 'I am: <strong>looking for opportunities!</strong>'],
+    strings: ['Hi! ✋', 'I am: Yero &copy;', 'I am: <strong>an aspiring software engineer 🤓</strong>', 'I am: <strong>looking for opportunities! 🤙</strong>'],
     // stringsElement: '#typed-strings',
     typeSpeed: 20,
     backSpeed: 20,
     startDelay: 1000,
     smartBackspace: true,
     loop: true,
-    loopCount: Infinity,
+    loopCount: 3,
   });
 });
