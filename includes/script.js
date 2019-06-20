d5906640415cb912df32100b1104ba33c3dd699c
yero <yeropharaoh@Yeros-MacBook-Pro.local>
2019-06-21 00:29:05 +0200

    homepage improvements + yieldr pages further developed

--- includes/script.js
+++ includes/script.js
@@ -13,17 +13,32 @@ function scrollAnchor (desiredHeight) {
 
 $(document).ready(() => {
   scrollAnchor(10);
+  
+  // HOVER
+  let sourceSwap = function () {
+    let $this = $(this);
+    let newSource = $this.data('alt-src');
+    $this.data('alt-src', $this.attr('src'));
+    $this.attr('src', newSource);
+  }
+  $(function() {
+    $('img[data-alt-src].proj-img').each(function() { 
+        new Image().src = $(this).data('alt-src'); 
+    }).hover(sourceSwap, sourceSwap); 
+  });
+
+});
 
 //TYPED.js
 document.addEventListener('DOMContentLoaded', function() {
-  var typed = new Typed('#typed', {
-    strings: ['Hi! ✋', 'I am: Yero &copy;', 'I am: <strong>an aspiring software engineer 🤓</strong>', 'I am: <strong>looking for opportunities! 🤙</strong>'],
+  let typed = new Typed('#typed', {
+    strings: ['Hi! ✋', 'I am: Yero &copy;', 'I am: <strong>a front-end software engineer 🤓</strong>', 'I am: <strong>looking for opportunities! 🤙</strong>'],
     // stringsElement: '#typed-strings',
     typeSpeed: 20,
     backSpeed: 20,
     startDelay: 1000,
     smartBackspace: true,
     loop: true,
-    loopCount: 3,
+    loopCount: 2,
   });
 });
